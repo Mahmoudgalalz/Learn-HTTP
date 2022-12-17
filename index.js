93b09599c778cb7b070847094aaeb9ac084295e5
Mahmoud Galal <ssss.aaa2002@gmail.com>
2022-12-17 03:00:29 +0200

    fetching the json file locally

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 const input=document.getElementById("input")
 const btn=document.getElementById('btn');
-
+const text=document.getElementById('text')
 
 function search(){
     const code=input.value;
-    const data=getData(code);
+    getData(code);
     const img=`https://http.cat/${code}`
-    putData(img,data);
+    putData(img);
 }
 
 function getData(code){
+
     fetch(`../public/Http-data/${code}.json`).then(response => {
         return response.json();
       }).then(data => {
-        return data;
+        textPut(data.Content)
       }).catch(err => {
         fetch(`../public/Http-data/default.json`).then(response => {
             return response.json();
           }).then(data => {
-            return data;
+            textPut(data.Content)
         });
       });
+
 }
-function putData(img,data){
+function putData(img){
     document.getElementById("img").src=img;
+    
+}
+function textPut(data){
+    text.innerHTML=data;
 }
 btn.addEventListener('click',search);
